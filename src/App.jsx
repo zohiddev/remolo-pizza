47851a640976a85d80a028e19f17ca7863dd6bf4
Satipov Akbar <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-06-20 13:08:07 +0500

    addc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import { Layout } from "./components";
 
 function App() {
-  return <Layout />;
+  // return <Layout />;
+
+
+  const [state, setState] = useState(
+    {
+      count1: 1,
+      count2: 19,
+      count3: 1,
+    },
+  
+  )
+  return (
+    <>
+      <button onClick={() => setState({ ...state , count1: state.count1 + 1 })}>Click me { state.count1}</button>
+      <button onClick={() => setState({ ...state , count2: state.count2 + 1 })}>Click me { state.count2}</button>
+      <button onClick={() => setState({ ...state , count3: state.count3 + 1 })}>Click me { state.count3}</button>
+    </>
+  )
 }
 
 export default App;
